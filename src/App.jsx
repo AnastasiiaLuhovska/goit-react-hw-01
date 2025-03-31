e5b4b4a71ad65138f60c9b0db9ec18667551a7a6
Stacy <stacyxberlin@gmail.com>
2025-03-31 20:12:21 +0200

    first task

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import userData from './userData.json'
+import Profile from "./components/Profile.jsx";
+
+function App() {
+  return (
+    <>
+        <Profile name={userData.username}
+                 tag={userData.tag}
+                 location={userData.location}
+                 image={userData.avatar}
+                 stats={userData.stats}/>
+    </>
+  )
+}
+
+export default App
